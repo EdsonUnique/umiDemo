0eeed64fc469013f5d505bb9e69c171e6eec32ea
Edson <3287320990@qq.com>
2019-08-26 10:06:06 +0800

    管理端

---
+++ client-admin/src/pages/manageBook/addBook.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout/lib/index';
+
+class AddBook extends Component {
+  render() {
+    return (
+      <PageHeaderWrapper>
+        <div>添加图书</div>
+      </PageHeaderWrapper>
+    );
+  }
+}
+
+export default AddBook;
---
+++ client-admin/src/pages/manageBook/viewBooks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout/lib/index';
+
+class ViewBooks extends Component {
+  render() {
+    return (
+      <PageHeaderWrapper>
+        <div>查看图书</div>
+      </PageHeaderWrapper>
+    );
+  }
+}
+
+export default ViewBooks;
