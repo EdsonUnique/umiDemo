8727aa1e03b8da9ceaf29e9a8dc0ee93d5cb6f9b
Edson <3287320990@qq.com>
2019-08-26 09:28:05 +0800

    管理端

---
+++ client-admin/config/defaultSettings.js
@@ -0,0 +1,16 @@
+export default {
+  navTheme: 'light',
+  primaryColor: '#1890FF',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: '书同友图书管理',
+  pwa: false,
+  iconfontUrl: '',
+};
